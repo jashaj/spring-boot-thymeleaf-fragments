26ed4e4a3108b2f62985e7c6e96e874886e62a0b
Jasha Joachimsthal <jashaj@gmail.com>
2016-04-16 10:50:53 +0200

    Init project with index page

---
+++ src/main/java/eu/jasha/HomeController.java
@@ -0,0 +1,13 @@
+package eu.jasha;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "")
+    public String getHome() {
+        return "index";
+    }
+}
---
+++ src/main/java/eu/jasha/SpringBootThymeleafFragmentsApplication.java
@@ -0,0 +1,12 @@
+package eu.jasha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafFragmentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootThymeleafFragmentsApplication.class, args);
+    }
+}
