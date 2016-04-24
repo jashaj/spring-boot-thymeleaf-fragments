c1497255206a6868a48e441bc939bf1d4972b998
Jasha Joachimsthal <jashaj@gmail.com>
2016-04-24 15:51:57 +0200

    Store preferred UI

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -14,28 +14,20 @@
  *   limitations under the License.
  */
 
-(function ($) {
+(function ($, window) {
+    var UI_MODES = {
+        ENRICHED: 'enriched',
+        PLAIN: 'plain'
+    };
+    var uiMode = UI_MODES.ENRICHED;
+    var uiModeStorageKey = 'uiMode';
+
     function initInlineEditable() {
         $.fn.editable.defaults.mode = 'inline';
         $('.inline-editable').editable();
     }
 
-    function initDraggableModals() {
-        $('.modal-dialog').draggable({
-            handle: ".modal-header"
-        });
-    }
-
-    function openEditFormInModal() {
-        // change all city-link links into modal triggers, but don't let Bootstrap do its magic loading the content into the
-        // modal-content div because it messes up the UI.
-        $('.city-link')
-            .attr('role', 'button')
-            .attr('data-toggle', 'modal')
-            .attr('data-target', '#cityModal')
-            .attr('data-remote', 'false')
-            .addClass('btn btn-default');
-
+    function initModalEventHandlers() {
         // loads content into .modal-body when the modal is shown
         var $cityModal = $('#cityModal');
         $cityModal.on('show.bs.modal', function (e) {
@@ -51,30 +43,81 @@
             e.preventDefault();
             $cityModal.modal('hide');
         });
+
+        // Make modals draggable
+        $('.modal-dialog').draggable({
+            handle: ".modal-header"
+        });
     }
 
-    function addButtonToRemoveEnhancedUx() {
-        // For demo: add a button to remove the modal magic
-        var $removeMagicBtn = $('<button></button>')
-            .addClass('btn btn-default')
-            .attr('type', 'button')
-            .text('Remove all magic')
-            .on('click', function () {
-                $(this).remove();
-                $('.inline-editable').editable('destroy');
-                $('.city-link')
-                    .removeAttr('role')
-                    .removeAttr('data-toggle')
-                    .removeAttr('data-target')
-                    .removeAttr('data-remote')
-                    .removeClass('btn btn-default');
-            });
-        $('#cities').after($removeMagicBtn);
+    function uiStandardHtml() {
+        $('.inline-editable').editable('destroy');
+        $('.city-link')
+            .removeAttr('role')
+            .removeAttr('data-toggle')
+            .removeAttr('data-target')
+            .removeAttr('data-remote')
+            .removeClass('btn btn-default');
     }
 
+    function uiEnriched() {
+        initInlineEditable();
 
-    openEditFormInModal();
-    initDraggableModals();
-    initInlineEditable();
-    addButtonToRemoveEnhancedUx();
-})(jQuery);
+        // change all city-link links into modal triggers, but don't let Bootstrap do its magic loading the content into the
+        // modal-content div because it messes up the UI.
+        $('.city-link')
+            .attr('role', 'button')
+            .attr('data-toggle', 'modal')
+            .attr('data-target', '#cityModal')
+            .attr('data-remote', 'false')
+            .addClass('btn btn-default');
+    }
+
+    function toggleUi() {
+        var uiMode = getUiMode();
+        if (uiMode == UI_MODES.ENRICHED) {
+            uiStandardHtml();
+            setUiMode(UI_MODES.PLAIN)
+        } else {
+            uiEnriched();
+            setUiMode(UI_MODES.ENRICHED);
+        }
+    }
+
+    function addButtonToToggleUx() {
+        var $toggleUiBtn = $('<a></a>')
+            .attr('role', 'button')
+            .text('Toggle UI')
+            .on('click', toggleUi);
+        $('#ui-toggle').html($toggleUiBtn).removeClass('hidden');
+    }
+
+    function getUiMode() {
+        if (!!window.localStorage) {
+            var storedMode = window.localStorage.getItem(uiModeStorageKey);
+            if (storedMode != undefined) {
+                uiMode = storedMode;
+            }
+        }
+        return uiMode;
+    }
+
+    function setUiMode(_uiMode) {
+        if (!!window.localStorage) {
+            window.localStorage.setItem(uiModeStorageKey, _uiMode);
+        } else {
+            uiMode = _uiMode;
+        }
+    }
+
+    function initUi() {
+        var preferredUxMode = getUiMode();
+        if (preferredUxMode == UI_MODES.ENRICHED) {
+            uiEnriched();
+        }
+        initModalEventHandlers();
+        addButtonToToggleUx();
+    }
+
+    initUi();
+})(jQuery, window);
