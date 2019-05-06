04877a03fd0d58f961efb1224fff25213a9109e4
Jasha Joachimsthal <jashaj@gmail.com>
2019-05-06 14:14:49 +0200

    Use more explicit method for mapping the request

--- src/main/java/eu/jasha/demo/sbtfragments/CityController.java
+++ src/main/java/eu/jasha/demo/sbtfragments/CityController.java
@@ -1,11 +1,11 @@
 /*
- *   Copyright 2018-2019 Jasha Joachimsthal
+ *   Copyright 2019 Jasha Joachimsthal
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
  *   You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *       https://www.apache.org/licenses/LICENSE-2.0
  *
  *   Unless required by applicable law or agreed to in writing, software
  *   distributed under the License is distributed on an "AS IS" BASIS,
@@ -56,7 +56,7 @@ public class CityController {
   @Resource
   private CityDao cityDao;
 
-  @RequestMapping
+  @GetMapping
   public String overview(ModelMap modelMap) {
     modelMap.addAttribute(MODEL_ATTRIBUTE_CITIES, cityDao.getAll());
     return VIEW_CITIES;
--- src/main/java/eu/jasha/demo/sbtfragments/HomeController.java
+++ src/main/java/eu/jasha/demo/sbtfragments/HomeController.java
@@ -1,11 +1,11 @@
 /*
- *   Copyright 2018 Jasha Joachimsthal
+ *   Copyright 2019 Jasha Joachimsthal
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
  *   You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *       https://www.apache.org/licenses/LICENSE-2.0
  *
  *   Unless required by applicable law or agreed to in writing, software
  *   distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,14 +17,14 @@
 package eu.jasha.demo.sbtfragments;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
   static final String VIEW_INDEX = "pages/index";
 
-  @RequestMapping(value = "")
+  @GetMapping(value = "")
   public String getHome() {
     return VIEW_INDEX;
   }
