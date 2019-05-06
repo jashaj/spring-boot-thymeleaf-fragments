0bc80a9a88193a81cef64c12d5e9670a17774375
Jasha Joachimsthal <jashaj@gmail.com>
2019-05-06 14:14:20 +0200

    Update Spring Boot and use it for dependency management

--- src/test/java/eu/jasha/demo/sbtfragments/CityControllerTest.java
+++ src/test/java/eu/jasha/demo/sbtfragments/CityControllerTest.java
@@ -1,11 +1,11 @@
 /*
- *   Copyright 2018 Jasha Joachimsthal
+ *   Copyright 2019 Jasha Joachimsthal
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
  *   You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *       https://www.apache.org/licenses/LICENSE-2.0
  *
  *   Unless required by applicable law or agreed to in writing, software
  *   distributed under the License is distributed on an "AS IS" BASIS,
@@ -37,7 +37,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.view.RedirectView;
 
