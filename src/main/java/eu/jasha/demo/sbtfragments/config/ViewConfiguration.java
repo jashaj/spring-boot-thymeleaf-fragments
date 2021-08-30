1e6af53e68d3b9359bdcc5351cb96f80a652201f
Jasha Joachimsthal <jashaj@gmail.com>
2021-08-30 19:10:02 +0200

    Update import

--- src/main/java/eu/jasha/demo/sbtfragments/config/ViewConfiguration.java
+++ src/main/java/eu/jasha/demo/sbtfragments/config/ViewConfiguration.java
@@ -16,23 +16,22 @@
 
 package eu.jasha.demo.sbtfragments.config;
 
+import ch.mfrey.thymeleaf.extras.with.WithDialect;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import ch.mfrey.thymeleaf.extras.with.WithDialect;
-import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 @Configuration
 public class ViewConfiguration {
 
-  @Bean
-  public LayoutDialect layoutDialect() {
-    return new LayoutDialect();
-  }
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
 
-  @Bean
-  public WithDialect withDialect() {
-    return new WithDialect();
-  }
+    @Bean
+    public WithDialect withDialect() {
+        return new WithDialect();
+    }
 
 }
